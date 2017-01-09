fae04f4f9c45fba61d426e3a32bd0343ba1ae574
Ana Aguilar Alonso <akiana@MacBook-Pro-de-Ana.local>
2017-01-09 22:05:27 +0100

    add load entity object

--- app/src/main/java/com/einao/ordersapp/data/network/firebase/OrdersNetworkDataSourceFirebase.java
+++ app/src/main/java/com/einao/ordersapp/data/network/firebase/OrdersNetworkDataSourceFirebase.java
@@ -2,6 +2,8 @@ package com.einao.ordersapp.data.network.firebase;
 
 import android.util.Log;
 
+import com.einao.ordersapp.data.entities.LoadEntity;
+import com.einao.ordersapp.data.entities.LoadsEntity;
 import com.einao.ordersapp.data.network.common.EndpointConstants;
 import com.einao.ordersapp.data.network.OrdersNetworkDataSource;
 import com.google.firebase.database.DataSnapshot;
@@ -9,6 +11,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.Iterator;
 
 
 public class OrdersNetworkDataSourceFirebase implements OrdersNetworkDataSource {
@@ -31,8 +35,14 @@ public class OrdersNetworkDataSourceFirebase implements OrdersNetworkDataSource 
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                //String value = dataSnapshot.getValue(String.class);
                 Log.d(this.getClass().getName(), dataSnapshot.toString());
+
+                LoadsEntity loadsEntity = new LoadsEntity();
+                Iterator<DataSnapshot> iterator = dataSnapshot.getChildren().iterator();
+                while (iterator.hasNext()){
+                    LoadEntity loadEntity = iterator.next().getValue(LoadEntity.class);
+                    loadsEntity.add(loadEntity);
+                }
             }
 
             @Override
